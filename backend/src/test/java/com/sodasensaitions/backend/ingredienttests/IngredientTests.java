a12618948e053d9fb90c8fd878e6e2ef9d172814
Jacob Atkinson <jacobatkinson200@gmail.com>
2024-11-18 13:16:40 -0700

    Add tests for inventory/available endpoint

--- backend/src/test/java/com/sodasensaitions/backend/ingredienttests/IngredientTests.java
+++ backend/src/test/java/com/sodasensaitions/backend/ingredienttests/IngredientTests.java
@@ -20,7 +20,10 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -80,6 +83,71 @@ public class IngredientTests {
 
     Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
     Assert.assertArrayEquals(ingredientResponse, ingredientsFromDb);
+  }
+
+  @Test
+  public void testGetAvailableIngredientsWithoutAvailable() {
+    Account account = new Account("getAvailableIngredients", "testGetAvailableIngredients@usu.edu", "mySecretPassword", "John", "Doe");
+    ResponseEntity<AuthenticationResponse> responseEntity = registeringUtils.tryRegistering(account, HttpStatus.OK);
+    ingredientRepository.deleteAll();
+    Ingredient[] ingredientsToSave = {new Ingredient("test1", 0, 0, true), new Ingredient("test2", 0, 1, true), new Ingredient("test3", 0, 2, false)};
+    ingredientRepository.saveAll(Arrays.asList(ingredientsToSave));
+
+    //Verify that all the ingredients are returned from the database
+    AuthenticationResponse authenticationResponse = responseEntity.getBody();
+    Assert.assertNotNull(authenticationResponse);
+    String token = authenticationResponse.getAccessToken();
+    Assert.assertNotNull(token);
+
+    String url = myURL() + "/inventory/available";
+    HttpHeaders headers = new HttpHeaders();
+    headers.setBearerAuth(token);
+    HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+    ResponseEntity<String> response = this.restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
+
+    Ingredient[] ingredientResponse = gson.fromJson(response.getBody(), Ingredient[].class);
+    Optional<List<Ingredient>> ingredientsOptional = ingredientRepository.findByQuantityGreaterThan(0);
+    List<Ingredient> ingredientsFromDb = new ArrayList<Ingredient>();
+
+    if (ingredientsOptional.isPresent()) {
+        ingredientsFromDb = ingredientsOptional.get();
+    }
+
+    Assert.assertArrayEquals(ingredientsFromDb.toArray(), new ArrayList<Ingredient>().toArray());
+
+  }
+
+  @Test
+  public void testGetAvailableIngredientsWithAvailable() {
+    Account account = new Account("getAvailableIngredientsWithAvailable", "testGetAvailableIngredientsWithAvailable@usu.edu", "mySecretPassword", "John", "Doe");
+    ResponseEntity<AuthenticationResponse> responseEntity = registeringUtils.tryRegistering(account, HttpStatus.OK);
+    ingredientRepository.deleteAll();
+    Ingredient[] ingredientsToSave = {new Ingredient("test1", 0, 0, true), new Ingredient("test2", 0, 1, true), new Ingredient("test3", 0, 2, false)};
+    ingredientRepository.saveAll(Arrays.asList(ingredientsToSave));
+
+    //Verify that all the ingredients are returned from the database
+    AuthenticationResponse authenticationResponse = responseEntity.getBody();
+    Assert.assertNotNull(authenticationResponse);
+    String token = authenticationResponse.getAccessToken();
+    Assert.assertNotNull(token);
+
+    String url = myURL() + "/inventory/available";
+    HttpHeaders headers = new HttpHeaders();
+    headers.setBearerAuth(token);
+    HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+    ResponseEntity<String> response = this.restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
+    Ingredient[] ingredientResponse = gson.fromJson(response.getBody(), Ingredient[].class);
+    Optional<List<Ingredient>> ingredientsOptional = ingredientRepository.findByQuantityGreaterThan(0);
+    List<Ingredient> ingredientsFromDb = new ArrayList<Ingredient>();
+
+    if (ingredientsOptional.isPresent()) {
+        ingredientsFromDb = ingredientsOptional.get();
+    }
+
+    Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+    Assert.assertArrayEquals(ingredientResponse, ingredientsFromDb.toArray());
   }
 
 
